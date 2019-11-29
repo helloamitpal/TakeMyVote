cae7880c75f9840314a27714cbec8aa429806c1f
IN22913318 <amit.pal@tesco.com>
2019-11-29 21:31:25 +0530

    broken: Added required dependencies for router and redux

---
+++ src/config/default.js
@@ -0,0 +1,3 @@
+const config = {
+    STORAGE_KEY: 'TakeMyVote'
+};
--- src/wrapper/App.js
+++ src/wrapper/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
-import Header from '../components/header';
+import Router from '../router';
+import config from '../config';
 
-import './App.css';
+import './app.css';
 
-const App = () => {
-  return (
-    <div className="app-container">
-      <Header />
-    </div>
-  );
-};
+class App extends Component {
+    constructor() {
+        super();
+        const { store } = configureStore()(this.onRehydrate);
+        this.state = {
+            store,
+            rehydrated: false
+        };
+    }
+
+    onRehydrate = () => {
+        this.setState({ rehydrated: true });
+    }
+
+    render() {
+        const { store, rehydrated } = this.state;
+
+        const content = rehydrated ? (
+            <div className="app-container">
+                <Provider store={store} key={config.STORAGE_KEY}>
+                    <BrowserRouter>
+                        <Router />
+                    </BrowserRouter>
+                </Provider>
+            </div>
+        ) : null;
+
+        return content;
+    }
+}
 
 export default App;
